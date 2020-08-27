3f230dba042413a95284dbce156f42433fd83e3b
ulia <sivonenkoyulia@gmail.con>
2020-08-27 06:07:50 +0300

    m-3

---
+++ js/task-5.js
@@ -0,0 +1,29 @@
+/*Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства. Возвращает массив значений определенного свойства prop из каждого объекта в массиве. */
+
+"use strict";
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const allPropValues = [];
+  for (const product of arr) {
+    if (prop in product) {
+      allPropValues.push(product[prop]);
+    }
+  }
+  return allPropValues;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
